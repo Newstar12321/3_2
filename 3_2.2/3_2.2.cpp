5675415cc66bd4b30b002e4e96aa9385e1c3da2c
Newstar12321 <tazeev.zhenya@mail.ru>
2023-10-10 00:16:07 +0700

    new

---
+++ 3_2.2/3_2.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int a, b, c;
+    cout << "Введите координаты точек a, b, c:" << endl;
+    cin >> a >> b >> c;
+    // Сначала я просто добавил в конце else (если максимальная длинна не находилась)
+    if ((a == b) || (a == c) || (b == c))
+    {
+        cout << "Все координаты должны быть различны";
+        return 1;
+    }
+    int ab = abs(a - b);
+    int bc = abs(b - c);
+    int ac = abs(a - c);
+    cout << "ab = " << ab << endl << "bc = " << bc << endl << "ac = " << ac << endl;
+    if((ab > bc) && (ab > ac))
+    {
+        cout << "Максимальная длина отрезков ab = " << ab;
+    }
+    else if ((bc > ab) && (bc > ac))
+    {
+        cout << "Максимальная длина отрезков bc = " << bc;
+    }
+    if ((ac > bc) && (ac > ab))
+    {
+        cout << "Максимальная длина отрезков ac = " << ac;
+    }
+}
